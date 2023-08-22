a1744891914531f083d44ec4b854d73f04cb726d
as4s4hetic <hyperbolicfish@outlook.com>
2023-08-23 08:19:38 +1000

    rename

--- blorg.el
+++ blorg.el
@@ -3,18 +3,19 @@
 (org-html-export-to-html)
 (kill-buffer (current-buffer))
  
-;; export blogs
-(defun export-blog (f)
+;; org to html helpers
+(defun export-file (f)
   (save-excursion
     (find-file f)
     (org-html-export-to-html)
     (kill-buffer (current-buffer))))
 
-(defun export-blogs (d)
-  (mapc 'export-blog
+(defun export-dir (d)
+  (mapc 'export-file
 	(directory-files d t ".org")))
 
-(export-blogs "./blogs")
+;; export blogs
+(export-dir "./blogs")
     
 
 
